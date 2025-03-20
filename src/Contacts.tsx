ac8558145a9924a5fd68e3fa4791e27544eee540
Abhishek Kakolla <abhishekkakolla1@gmail.com>
2025-03-19 23:46:27 -0700

    moved contacts to top

--- src/Contacts.tsx
+++ src/Contacts.tsx
@@ -6,7 +6,7 @@ function Contacts() {
 
 
 
-    return <div className="absolute bottom-5 right-10 text-white flex w-20">
+    return <div className="absolute top-12 right-10 text-white flex w-20">
         <a target="_blank" href="https://github.com/kakolla" className="flex-auto fa fa-github"></a>
         <a target="_blank" href="https://www.linkedin.com/in/kakolla" className="flex-auto fa fa-linkedin-square"></a>
         <a href="mailto:kakolla@usc.edu" className="fa fa-envelope color-white"></a>
