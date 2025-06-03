1abb038e35e2d66bf4465a9e66d852c95ef35321
kakolla <abhishekkakolla1@gmail.com>
2025-06-02 21:18:26 -0400

    update fp

--- src/Home.tsx
+++ src/Home.tsx
@@ -9,12 +9,19 @@ function Home({ pageState }: Props) {
         return <div className="highlighted-text animate-fade absolute top-2/3 left-20  
         text-white overflow-y-auto no-scrollbar bottom-10
         max-md:w-3/4 max-md:bottom-10">
-            <h1 className="text-2xl">Hi there, I'm <p className="inline text-lime-600">Abi</p></h1>
+            <h1 className="text-2xl">Hi there, I'm <p className="inline text-lime-600">Abi 👽</p></h1>
             <h1 className="pt-4 max-w-xl">I'm a Computer Science student at USC enthusiastic about
                 machine learning, neuroscience, and developing creative solutions to real-world problems.
             </h1>
             <h1 className="pt-4 max-w-xl">
-                I'm currently performing research at the Center for Neural Engineering @ USC and exploring GraphRAG-based architectures for improving LLM reasoning.
+                I'm currently designing and evaluating enterprise FinOps models at 
+                <p className="inline text-sky-600"> 
+                <a href="https://stitcher.ai/" target="_blank"> StitcherAI</a></p> and 
+                performing research on improving LLM reasoning at the <p className="inline text-sky-600">
+                <a href="https://itemsusc.org/" target="_blank">
+                Center for Neural 
+                Engineering @ USC
+                </a></p>
             </h1>
 
 
