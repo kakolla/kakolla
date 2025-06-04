05f6b4554c1faca3c3deede263bbddcd89adadf2
kakolla <abhishekkakolla1@gmail.com>
2025-06-04 18:32:57 -0400

    react router for posts with slugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react';
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import ThreeScene from './ThreeScene.tsx'
 import Title from './Title.tsx'
 import NavBar from './NavBar.tsx'
-import Home from './Home.tsx';
-import About from './About.tsx';
-import Contacts from './Contacts.tsx';
-import { Analytics } from "@vercel/analytics/react"
-import Loading from './Loading.tsx';
-import Notes from './Notes.tsx';
+import Home from './Home.tsx'
+import About from './About.tsx'
+import Contacts from './Contacts.tsx'
+import { Analytics } from '@vercel/analytics/react'
+import Loading from './Loading.tsx'
+import Notes from './Notes.tsx'
+import PostPage from './PostPage.tsx'
 
-
-function App() {
-    const [state, setState] = useState<string>("home");
-    const [endLoadingScreen, setEndLoadingScreen] = useState<boolean>(false);
-
-    return <>
-        <Analytics /> 
-        <Loading endLoadingScreen={endLoadingScreen}/>
-        <Title text="Abi Kakolla" pageState={state} />
-        <Home pageState={state} />
-        <Notes pageState={state} />
-        <About pageState={state} />
-        <ThreeScene pageState={state} setPage={setState} setEndLoadingScreen={setEndLoadingScreen}/>
-        <NavBar pageState={state} onClickFunc={setState}/>
-        <Contacts />
-    </>
-
-
-
-
+function MainPages({ pageState }: { pageState: string }) {
+    return (
+        // Main navigable pages
+        <div>
+            <Home pageState={pageState} />
+            <Notes pageState={pageState} />
+            <About pageState={pageState} />
+            <Contacts />
+        </div>
+    )
 }
 
-export default App;
+function App() {
+    const [state, setState] = useState("home")
+    const [endLoadingScreen, setEndLoadingScreen] = useState(false)
+
+    return (
+        <Router>
+            <Analytics />
+            <Loading endLoadingScreen={endLoadingScreen} />
+            {/* Main components */}
+            <Title text="Abi Kakolla" />
+            <ThreeScene pageState={state} setPage={setState} setEndLoadingScreen={setEndLoadingScreen} />
+            <NavBar pageState={state} onClickFunc={setState} />
+
+            {/* Route to posts on the note page */}
+            <Routes>
+                <Route path="/" element={<MainPages pageState={state} />} />
+                <Route path="/:slug" element={<PostPage />} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default App
