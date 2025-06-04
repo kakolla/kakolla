3785c74f8282bbd82ab120ffab6faf43c96ff4b2
kakolla <abhishekkakolla1@gmail.com>
2025-06-04 18:32:31 -0400

    add notes to navbar

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,21 +1,27 @@
-// import React from 'react';
+import { useNavigate } from 'react-router-dom'
 
-
-interface Props
-{
-    pageState: string,
-    onClickFunc: any;
+interface Props {
+    pageState: string
+    onClickFunc: (newState: string) => void
 }
 
 function NavBar(props: Props) {
+    const navigate = useNavigate() // hook to navigate without reloading
 
+    const handleClick = (section: string) => {
+        navigate('/')              // route back to homepage
+        props.onClickFunc(section) // set the section (e.g., notes, about)
+    }
 
-    return <div className="absolute top-1/2 right-10 w-40 ">
-    <button className="mr-0 ml-auto text-white block font-bold" onClick={() => props.onClickFunc("home")}>home</button>
-    <button className="mr-0 ml-auto text-white block pt-2 font-bold" onClick={() => props.onClickFunc("stuff")}>projects</button>
-    <button className="mr-0 ml-auto text-white block pt-2 font-bold" onClick={() => props.onClickFunc("notes")}>notes</button>
-    <button className="mr-0 ml-auto text-white block pt-2 font-bold" onClick={() => props.onClickFunc("about")}>about me</button>
-    </div>;
+    return (
+        // Navbar buttons
+        <div className="z-50 absolute top-1/2 right-10 w-40">
+            <button className="mr-0 ml-auto text-white block font-bold" onClick={() => handleClick("home")}>home</button>
+            <button className="mr-0 ml-auto text-white block pt-2 font-bold" onClick={() => handleClick("stuff")}>projects</button>
+            <button className="mr-0 ml-auto text-white block pt-2 font-bold" onClick={() => handleClick("notes")}>notes</button>
+            <button className="mr-0 ml-auto text-white block pt-2 font-bold" onClick={() => handleClick("about")}>about me</button>
+        </div>
+    )
 }
 
 export default NavBar;
