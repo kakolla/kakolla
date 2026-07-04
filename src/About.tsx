7681fa564e8c4f40f50d739781ae6ff4aa98ba32
kakolla <abhishekkakolla1@gmail.com>
2026-07-04 16:35:42 -0700

    upd

--- src/About.tsx
+++ src/About.tsx
@@ -7,37 +7,38 @@ interface Props {
 function About({ pageState }: Props) {
     // let text = `
     // My name's Abi Kakolla. 
-    
-    
+
+
     // `
     if (pageState === "about") {
         return <div className="highlighted-text animate-fade absolute top-2/3 left-20  
         text-white overflow-y-auto no-scrollbar bottom-10
         max-md:w-3/4 max-md:bottom-10">
             <p className="pt-2 max-w-xl">
-            ─── ･ about me ･ ───
+                ─── ･ about me ･ ───
             </p>
             <p className="pt-2 max-w-xl">
-                My interest for computing and all things engineering started when I was 8 when I learned how to use VBScript
-                to create my first gui-based quiz game. Ever since then, I've loved using computers to build 
-                creative solutions for all kinds of problems.
+                My interest for computing and engineering started when I was 8 when I learned how to use VBScript
+                to create my first gui-based quiz game on my mom's windows Vista. I've loved taking apart laptops, modifying firmware,
+                messing with operating systems, and creating things at the intersection of hardware and software.
+
             </p>
 
             <br />
             <p className="pt-2 max-w-xl">
                 Currently, I'm excited about the intersection of neuroscience, machine learning, and computer science. I've spent
-                the last few years performing research at the Center for Neural Engineering @ USC, working for startups, participating in hackathons, and 
+                the last few years performing research at the Center for Neural Engineering @ USC, working for startups, participating in hackathons, and
                 trying to get as low level as I can with computer science.
             </p>
 
             <br />
-            <p className="pt-2 max-w-xl">When I'm not learning, I love to do calisthenics, listen to rnb music, and play horror games.
+            <p className="pt-2 max-w-xl">When I'm not learning, I love to do calisthenics, listen to rnb music, and spend way too much time editing my dotfiles.
                 If you're also on this lifelong journey of learning or resonate with anything, please connect with me!
             </p>
             <br />
             <h2 className="pt-2 max-w-xl"><i>
-                This site is still under construction, so please don't mind the incompleteness!
-                </i>
+                P.S. the model of Toronto and the rest of the website were created by me in Blender and with TS (not AI).
+            </i>
             </h2>
 
 
