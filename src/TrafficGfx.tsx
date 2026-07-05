5745f8cb12ac5ed17f56cd55032b2d0f8d048cd1
kakolla <abhishekkakolla1@gmail.com>
2026-07-05 15:20:50 -0700

    disable frustum issue with cars

--- src/TrafficGfx.tsx
+++ src/TrafficGfx.tsx
@@ -56,6 +56,8 @@ export async function loadTrafficLane(
             }
             instancedMesh.instanceMatrix.needsUpdate = true;
 
+            instancedMesh.frustumCulled = false;
+
             laneRefs.instancedMesh.current = instancedMesh;
             sceneRef.current.add(instancedMesh);
         }
